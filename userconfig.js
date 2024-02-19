aaf659ced406e9f0483a8ed752c7123ba9ac5f0a
Akan <megaquick11@gmail.com>
2024-02-19 16:07:20 -0600

    modified index, userconfig, and added some img

--- userconfig.js
+++ userconfig.js
@@ -31,9 +31,10 @@ const CONFIG = new Config({
   tabs: [
     {
       name: 'home',
-      background_url: 'src/img/banners/bg-10.png',
-      categories: [{
-        name: 'animanga fun',
+      background_url: 'src/img/banners/bg-8.png',
+      categories: [
+        {
+        name: 'most important',
         links: [
           {
             url: 'https://www.mangadex.org/',
@@ -44,19 +45,25 @@ const CONFIG = new Config({
           {
             url: 'https://anilist.co/home',
             name: 'anilist',
-            icon: 'src/img/icons/simple-icons--anilist.svg',
-            icon_color: '#64c1d3'
           },
           {
-            url: 'https://www.4chan.org/a/',
-            name: '/a/'
+            url: 'https://youtu.be/',
+            icon: 'brand-youtube',
+            icon_color: '#996767'
+          },
+        ],
+        name: '',
+        links: [
+          {
+
           },
         ]
-      }]
+      }
+    ],
     },
     {
       name: 'boards',
-      background_url: 'src/img/banners/bg-1.gif',
+      background_url: 'src/img/banners/bg-2.gif',
       categories: [{
         name: 'fun',
         links: [
@@ -71,9 +78,8 @@ const CONFIG = new Config({
             icon_color: '#64876d'
           },
           {
-            name: '/lit/',
-            url: 'https://4chan.org/lit/',
-            icon: 'book'
+            url: 'https://www.4chan.org/a/',
+            name: '/a/'
           },
           {
             name: '/v/',
@@ -82,9 +88,8 @@ const CONFIG = new Config({
             icon_color: '#8b647b'
           },
           {
-            name: '/p/',
-            url: 'https://4chan.org/p/',
-            icon: 'camera'
+            name: '/vt/',
+            url: 'https://4chan.org/vt/'
           }
         ]
       },
@@ -98,15 +103,74 @@ const CONFIG = new Config({
           {
             name: '/comfy/',
             url: 'https://anon.cafe/comfy/catalog.html'
+          },
+          {
+            name: '/p/',
+            url: 'https://4chan.org/p/',
+            icon: 'camera',
+            icon_color: '#6c849d'
           }
         ]
       },
+      {
+        name: 'music boards',
+        links: [
+          {
+            name: '/music/',
+            url: 'https://lainchan.org/music/',
+            icon: 'disc'
+          },
+          {
+            name: '/mu/',
+            url: 'https://boards.4channel.org/mu/',
+          }
+        ]
+      },
+      {
+        icon:'brand-reddit',
+        icon_color:'#d28860',
+        links: [
+          {
+            name:'r/piracy',
+            link:'https://www.reddit.com/r/Piracy',
+          },
+          {
+            name:'r/animepiracy',
+            link:'https://www.reddit.com/r/animepiracy',
+          },
+          {
+            name:'r/japanesepeopletwitter',
+            link:'https://www.reddit.com/r/japanesepeopletwitter',
+          }
+        ]
+      }
       ]
     },
     {
-      name: 'music',
-      background_url: 'src/img/banners/bg-2.gif',
+      name: 'entertainment',
+      background_url: 'src/img/banners/bg-15.png',
       categories: [
+        {
+          name: 'otaku',
+          links: [
+            {
+              name: 'anilist',
+              url: 'https://anilist.co',              
+            },
+            {
+              name: 'mal',
+              url: 'https://myanimelist.com/',              
+            },
+            {
+              name: 'mangadex',
+              url: 'https://mangadex.org/',   
+            },
+            {
+              name: 'nyaa',
+              url: 'https://nyaa.si/',   
+            },
+          ]
+        },
         {
           name: 'music services',
           links: [
@@ -116,7 +180,8 @@ const CONFIG = new Config({
               icon_color: '#c57750'
             },
             {
-              url: 'https://youtu.be/',
+              url: 'https://music.youtube.com/',
+              name: 'music',
               icon: 'brand-youtube',
               icon_color: '#996767'
             },
@@ -124,28 +189,19 @@ const CONFIG = new Config({
               url: 'https://r-a-d.io/',
               icon: 'radio'
             },
+            {
+              url: 'https://r-a-d.io/',
+              icon: 'radio'
+            },
           ]
         },
-        {
-          name: 'music boards',
-          links: [
-            {
-              name: '/music/',
-              url: 'https://lainchan.org/music/',
-              icon: 'disc'
-            },
-            {
-              name: '/mu/',
-              url: 'https://boards.4channel.org/mu/',
-            }
-          ]
-        }
       ]
     },
      {
       name: 'myself',
       background_url: 'src/img/banners/bg-5.jpg',
-      categories: [{
+      categories: [
+        {
         name: 'social',
         links: [
             {
@@ -165,6 +221,29 @@ const CONFIG = new Config({
               name: 'reddit',
               icon: 'brand-reddit',
               icon_color: '#c98264'             
+            },
+          ]
+        },
+        {
+          name: 'shopping',
+          links: [
+            {
+              url: 'https://www.amazon.com.mx',
+              name: 'amazon',
+              icon: 'brand-amazon',
+              icon_color: '#d7b965'            
+            },
+            {
+              url: 'https://www.aliexpress.com/',
+              name: 'aliexpress',
+              icon: 'shopping-cart',
+              icon_color: '##d77c65'            
+            },
+            {
+              url: 'https://mx.ebay.com/',
+              name: 'ebay',
+              icon: 'basket-filled',
+              icon_color: '#65d778'            
             },
           ]
         }
